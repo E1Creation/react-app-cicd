7479aa1ee282cebe5bc2e09a6bf3e573ea00e735
Kurniawan Sudirman <kurniawanplusayahibu@gmail.com>
2022-10-26 12:29:56 +0700

    change sleep

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
-
+sleep 60
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
-sleep 60
 kill $(cat .pidfile)
 set +x
